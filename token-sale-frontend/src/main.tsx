1c118da68f44d072dc64471c4bad87dcc3dc192c
RAMKI14 <ramki.t@hotmail.com>
2025-07-03 10:07:44 +0100

    Added 'connect wallet button', a header tag

--- token-sale-frontend/src/main.tsx
+++ token-sale-frontend/src/main.tsx
@@ -1,10 +1,38 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "@rainbow-me/rainbowkit/styles.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import {
+  getDefaultConfig,
+  RainbowKitProvider
+} from "@rainbow-me/rainbowkit";
+import {
+  WagmiProvider,
+  http
+} from "wagmi";
+import { sepolia } from "wagmi/chains";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const config = getDefaultConfig({
+  appName: "Token Sale DApp",
+  projectId: import.meta.env.VITE_WC_PROJECT_ID,
+  chains: [sepolia],
+  transports: {
+    [sepolia.id]: http()
+  }
+});
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <App />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  </React.StrictMode>
+);

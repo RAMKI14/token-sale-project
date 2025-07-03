0e43972d36e9597af09dbd18463654aa1d41ae79
RAMKI14 <ramki.t@hotmail.com>
2025-07-03 20:09:36 +0100

    more changes, added some code to 'hardhat.config.ts', created '.env' file at the root and added MetaMask_Dev_API to SEPOLIA_RPC_URL, MetaMask_PrivateKey to PRIVATE_KEY. But '.env' is added to '.gitignore

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,15 @@
+import "dotenv/config";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "",
+      accounts: [process.env.PRIVATE_KEY || ""],
+    },
+  },
 };
 
 export default config;
